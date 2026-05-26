be461e0198459a560316056c9d1c23b5ab9abbc0
Louis Kapend <louiskapend2016@gmail.com>
2026-05-26 18:04:58 -0400

    sermons section enhanced

--- site-config.js
+++ site-config.js
@@ -6,7 +6,7 @@ window.TELEIOS_CONFIG = {
     siteUrl: 'https://teleioschurch.co.za',
     youtubeChannelId: 'UCIqka1KcckcYRK3DXynQbiw',
     youtubeChannelUrl: 'https://www.youtube.com/channel/UCIqka1KcckcYRK3DXynQbiw',
-    youtubeMaxVideos: 9,
+    youtubeMaxVideos: 50,
     /** Optional: public Google Calendar embed URL (iframe src) for events */
     googleCalendarEmbedUrl: ''
 };
